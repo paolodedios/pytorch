dd6e765937355506035ce0fa6d067bfaf22a57ad
angelayi <yiangela7@gmail.com>
2023-09-19 15:57:51 -0700

    [aotinductor] Skip benchmarks with control flow
    Since AOTInductor doesn't support control flow yet, we will skip over
    tests that are currently failing due to containing control flow in the
    code. Logs taken from https://hud.pytorch.org/benchmark/compilers?startTime=Tue%2C%2012%20Sep%202023%2022%3A56%3A40%20GMT&stopTime=Tue%2C%2019%20Sep%202023%2022%3A56%3A40%20GMT&granularity=hour&suite=torchbench&mode=inference&dtype=bfloat16&lBranch=main&lCommit=2c1554a0323107d821be3ff13df7833b9f0b960d&rBranch=main&rCommit=47be61e12bd51df27182343d312dc3df485d5559

--- benchmarks/dynamo/torchbench.py
+++ benchmarks/dynamo/torchbench.py
@@ -85,6 +85,15 @@ SKIP = {
     "clip",
 }
 
+SKIP_DUE_TO_CONTROL_FLOW = {
+    "cm3leon_generate",
+    "fastNLP_Bert",
+    "hf_Longformer",
+    "hf_Reformer",
+    "hf_T5_generate",
+    "opacus_cifar10",
+}
+
 SKIP_FOR_CPU = {
     "hf_T5_generate",  # OOMs
     "cm3leon_generate",  # model is CUDA only
@@ -310,6 +319,10 @@ class TorchBenchmarkRunner(BenchmarkRunner):
     @property
     def skip_multiprocess_models(self):
         return ONLY_MULTIPROCESS
+
+    @property
+    def skip_models_due_to_control_flow(self):
+        return SKIP_DUE_TO_CONTROL_FLOW
 
     def load_model(
         self,
