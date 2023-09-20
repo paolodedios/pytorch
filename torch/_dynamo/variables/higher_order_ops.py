300ff8816b1678f77c7f1090da248aa33fc809f6
ydwu4 <ydwu2014@gmail.com>
2023-09-20 14:36:32 -0700

    Replace node.meta source_fn with source_fn_stack (#108595)
    Summary:
    X-link: https://github.com/pytorch/executorch/pull/210
    
    A resubmit of https://github.com/pytorch/pytorch/pull/108447. Copy over the descriptions:
    
    This is a follow-up of the discussion in https://github.com/pytorch/pytorch/pull/108356, where we want to repalce source_fn with source_fn_stack
    
    Before this PR, for the following example:
    ```python
    backend = EagerAndRecordGraphs()
    
    torch.compile(backend=backend, fullgraph=True)
    def cond_f(pred, pred2, x, y):
        def true_fn(pred2, x, y):
            return x + y
    
        def false_fn(pred2, x, y):
            def true_fn2(x, y):
                return x.sin() - y.cos()
    
            def false_fn2(x, y):
                return x.cos() - y.sin()
    
            return control_flow.cond(pred2, true_fn2, false_fn2, (x, y))
    
        return control_flow.cond(pred, true_fn, false_fn, (pred2, x, y))
    ```
    The graph captured is shown below:
    ```python
    class GraphModule(torch.nn.Module):
        def forward(self, L_pred_ : torch.Tensor, L_pred2_ : torch.Tensor, L_x_ : torch.Tensor, L_y_ : torch.Tensor):
            l_pred_ = L_pred_
            l_pred2_ = L_pred2_
            l_x_ = L_x_
            l_y_ = L_y_
    
            cond_true_1 = self.cond_true_1
            cond_false_1 = self.cond_false_1
            cond = torch.ops.higher_order.cond(l_pred_, cond_true_1, cond_false_1, [l_pred2_, l_x_, l_y_]);  l_pred_ = cond_true_1 = cond_false_1 = l_pred2_ = l_x_ = l_y_ = None
            return (cond,)
    
        class GraphModule(torch.nn.Module):
            def forward(self, l_pred2_, l_x_, l_y_):
                add = l_x_ + l_y_;  l_x_ = l_y_ = None
                return add
    
        class GraphModule(torch.nn.Module):
            def forward(self, l_pred2_, l_x_, l_y_):
                cond_true_0 = self.cond_true_0
                cond_false_0 = self.cond_false_0
                cond = torch.ops.higher_order.cond(l_pred2_, cond_true_0, cond_false_0, [l_x_, l_y_]);  l_pred2_ = cond_true_0 = cond_false_0 = l_x_ = l_y_ = None
                return cond
    
            class GraphModule(torch.nn.Module):
                def forward(self, l_x_, l_y_):
                    sin = l_x_.sin();  l_x_ = None
                    cos = l_y_.cos();  l_y_ = None
                    sub = sin - cos;  sin = cos = None
                    return sub
    
            class GraphModule(torch.nn.Module):
                def forward(self, l_x_, l_y_):
                    cos = l_x_.cos();  l_x_ = None
                    sin = l_y_.sin();  l_y_ = None
                    sub = cos - sin;  cos = sin = None
                    return sub
    ```
    the source_fn for inner cond, sin, cos will be a (name, target) tuple:
    ```
    ('cond', <torch._ops.HigherOrderOperator object at xxx>)
    ('sin', 'sin')
    ('cos', 'cos')
    ('sub'. <built-in function sub>)
    ```
    
    After this pr, the source_fn_stack will be a list of (name, target) tuple. The bottom of stack is the end of the list.
    ```
    [('cond', <torch._ops.HigherOrderOperator object at xxx>), ('cond', <torch._ops.HigherOrderOperator object at xxx>)],
    [('cond', <torch._ops.HigherOrderOperator object at xxx>), ('cond', <torch._ops.HigherOrderOperator object at xxx>), ('sin', 'sin')],
    [('cond', <torch._ops.HigherOrderOperator object at xxx>), ('cond', <torch._ops.HigherOrderOperator object at xxx>), ('cos', 'cos')]
    [('cond', <torch._ops.HigherOrderOperator object at xxx>), ('cond', <torch._ops.HigherOrderOperator object at xxx>), ('sub', <built-in function sub>)]
    ```
    
    
    Test Plan:
    See added tests in test_higher_order_ops.py and modify existing test.
    Also updated bin by running:
    "buck2 run @//mode/dev-nosan fbcode//aibench/api:gen_test_files --config client.id=nuclide"
    
    cc voznesenskym penguinwu EikanWang jgong5 Guobing-Chen XiaobingSuper zhuhaozhe blzheng Xia-Weiwen wenzhe-nrv jiayisunx peterbell10 ipiszy ngimel yf225 chenyang78 kadeng muchulee8 aakhundov
    
    Reviewed By: angelayi
    
    Differential Revision: D48984986
    
    Pulled By: ydwu4

--- torch/_dynamo/variables/higher_order_ops.py
+++ torch/_dynamo/variables/higher_order_ops.py
@@ -153,6 +153,9 @@ def speculate_subgraph(
     checkpoint,
     description,
     *,
+    # source_target is the .value of HigherOrderOpVariable and is the
+    # target of the proxy that we created for the higherOrderOperator.
+    source_target=None,
     always_restore=False,
     enable_grad=False,
     # NOTE [Temporary argument `manually_set_subgraph_inputs`]
@@ -175,7 +178,7 @@ def speculate_subgraph(
         )
 
     try:
-        with tx.output.new_subtracer() as tracer:
+        with tx.output.new_subtracer(source_target) as tracer:
             args = validate_args_and_maybe_create_graph_inputs(
                 sub_args, tracer, tx, manually_set_subgraph_inputs
             )
@@ -431,6 +434,7 @@ class CondHigherOrderVariable(TorchHigherOrderOperatorVariable):
                 graph_checkpoint,
                 checkpoint,
                 "cond",
+                source_target=self.value,
             )
 
             if not isinstance(ret_val, TensorVariable):
@@ -583,6 +587,7 @@ class MapHigherOrderVariable(TorchHigherOrderOperatorVariable):
             tx.output.graph,
             checkpoint,
             "torch.ops.higher_order.map",
+            source_target=self.value,
         )
 
         body_nn_modules = tx.copy_graphstate().output.nn_modules
@@ -718,6 +723,7 @@ class FunctorchGradHigherOrderVariable(TorchHigherOrderOperatorVariable):
             graph_checkpoint,
             checkpoint,
             "torch.func.grad",
+            source_target=self.value,
             # See NOTE [HACK: Enable autograd while tracing function]
             enable_grad=True,
         )
@@ -929,6 +935,7 @@ class FunctorchVmapHigherOrderVariable(TorchHigherOrderOperatorVariable):
                 graph_checkpoint,
                 checkpoint,
                 "torch.vmap",
+                source_target=self.value,
             )
 
         body_name = add_subgraph(
@@ -1039,6 +1046,7 @@ class AutogradFunctionMethodHigherOrderVariable(TorchHigherOrderOperatorVariable
             graph_checkpoint,
             checkpoint,
             "the user-defined autograd.Function",
+            source_target=self.value,
             # Backwards should never, ever be stored!
             always_restore=always_restore,
             restore_side_effects=False,
@@ -1096,6 +1104,7 @@ class WrapHigherOrderVariable(TorchHigherOrderOperatorVariable):
             graph_checkpoint,
             checkpoint,
             description,
+            source_target=self.value,
             manually_set_subgraph_inputs=False,
         )
 
