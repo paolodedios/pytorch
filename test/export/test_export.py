60c42371aa37a2b56ec7a37132288deed174c1b0
Wuxun Zhang <wzhang@habana.ai>
2024-08-14 09:19:19 +0300

    fix lint and skip for non-strict export mode

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -6826,6 +6826,9 @@ def forward(self, x, y):
             if node.op == "call_function":
                 self.assertTrue(False)
 
+    @testing.expectedFailureNonStrict
+    @testing.expectedFailureTrainingIRToRunDecomp
+    @testing.expectedFailureTrainingIRToRunDecompNonStrict  # unbacked symint not tracked?
     @testing.expectedFailureSerDer  # T195866111
     def test_hints_wrapper(self):
         class M(torch.nn.Module):
