79318190148af931e01b290887cd04be8767d2ac
cyy <cyyever@outlook.com>
2024-11-07 09:12:41 +0800

    Use TORCH_DECLARE_XXX

--- torch/csrc/lazy/core/config.cpp
+++ torch/csrc/lazy/core/config.cpp
@@ -1,6 +1,5 @@
 #include <torch/csrc/lazy/core/config.h>
 
-// NOLINTBEGIN(misc-use-internal-linkage)
 C10_DEFINE_bool(torch_lazy_ir_debug, false, "Enable lazy tensor IR debugging");
 
 C10_DEFINE_bool(
