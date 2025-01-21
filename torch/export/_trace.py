54c8fd833810a7c160f67076b696a228ca364d25
Aaron Gokaslan <aaronGokaslan@gmail.com>
2025-01-21 11:50:34 -0500

    Fix lint

--- torch/export/_trace.py
+++ torch/export/_trace.py
@@ -850,9 +850,7 @@ def _get_non_persistent_buffers(mod: torch.nn.Module) -> Set[str]:
     result: Set[str] = set()
     for name, m in mod.named_modules(remove_duplicate=False):
         if name:
-            result.update(
-                f"{name}.{b}" for b in m._non_persistent_buffers_set
-            )
+            result.update(f"{name}.{b}" for b in m._non_persistent_buffers_set)
         else:
             result.update(m._non_persistent_buffers_set)
     return result
