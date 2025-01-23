2acb663af6db4305fc3eb0a3a66d896c493673bd
Pian Pawakapan <pianpwk@meta.com>
2025-01-22 17:42:04 -0800

    [draft_export] add LOC for data-dep error logging (#145443)
    Summary:
    
    maybe this is too much info, but it's difficult to go through old draft export reports where the stack trace is out of sync with the current codebase.
    
    This would be even more helpful for reports on torch-packaged models, but that requires some more work on stack trace processing
    
    Test Plan: .
    
    Differential Revision: D68534017

--- torch/export/_draft_export.py
+++ torch/export/_draft_export.py
@@ -54,6 +54,16 @@ def hash_stack(stack: list[dict[str, str]]) -> str:
     return ";".join(f'line: {s["line"]} filename: {s["filename"]}' for s in stack)
 
 
+def get_loc(filename: str, lineno: int) -> Optional[str]:
+    try:
+        with open(filename, "r") as f:
+            for i, line in enumerate(f):
+                if i == lineno - 1:
+                    return line.strip()
+    except FileNotFoundError:
+        return None
+
+
 class FailureReport:
     def __init__(
         self, failure_type: FailureType, data: dict[str, Any], xfail: bool = False
@@ -89,9 +99,12 @@ class FailureReport:
 """
 
         elif self.failure_type == FailureType.DATA_DEPENDENT_ERROR:
+            frame = self.data["stack"][-1]
+            loc = f"`{get_loc(str_to_filename[frame['filename']], frame['line'])}`" or ""
             return f"""Data dependent error.
-    When exporting, we were unable to figure out if the expression `{self.data["expr"]}` always holds.
-    This occurred at the following stacktrace: {prettify_stack(self.data["stack"], str_to_filename)}.
+    When exporting, we were unable to evaluate the value of `{self.data["expr"]}`.
+    This occurred at the following stacktrace: {prettify_stack(self.data["stack"], str_to_filename)}:
+        {loc}
     As a result, it was specialized to evaluate to `{self.data["result"]}`, and asserts were inserted into the graph.
 
     Please add `torch._check(...)` to the original code to assert this data-dependent assumption.
