00ca018bce7bf9a89547fc3528095e3dae82935e
Jeff Daily <jeff.daily@amd.com>
2025-02-21 18:30:57 +0000

    fix windows build error

--- aten/src/ATen/native/cuda/Indexing.cu
+++ aten/src/ATen/native/cuda/Indexing.cu
@@ -682,6 +682,9 @@ void index_put_with_sort_kernel(Tensor & self, const c10::List<std::optional<Ten
         // Adapt grid size to smaller virtual warp size:
         dim3 new_grid(ceil_div(num_indices, (int64_t) (indices_per_block * warp_size)), grid.y, grid.z);
         size_t smem_dups_size = indices_per_block * warp_size * sizeof(int64_t);
+#define KERNEL_LAUNCH_PARAMS new_grid, block, smem_dups_size, stream
+#else
+#define KERNEL_LAUNCH_PARAMS grid, block, 0, stream
 #endif
         // This implementation is faster with high amounts of duplicates but could overflow
         // if FP16 / BF16 is used
@@ -689,11 +692,7 @@ void index_put_with_sort_kernel(Tensor & self, const c10::List<std::optional<Ten
           expandedValue.scalar_type(),
           "indexing_backward_kernel_stride_1",
           AT_WRAP([&] {
-#ifdef USE_ROCM
-            indexing_backward_kernel_stride_1<scalar_t><<<new_grid, block, smem_dups_size, stream>>>
-#else
-            indexing_backward_kernel_stride_1<scalar_t><<<grid, block, 0, stream>>>
-#endif
+            indexing_backward_kernel_stride_1<scalar_t><<<KERNEL_LAUNCH_PARAMS>>>
             (
               sorted_indices.const_data_ptr<int64_t>(),
               orig_indices.const_data_ptr<int64_t>(),
@@ -718,6 +717,7 @@ void index_put_with_sort_kernel(Tensor & self, const c10::List<std::optional<Ten
           kHalf,
           kBool,
           kBFloat16);
+#undef KERNEL_LAUNCH_PARAMS
       } else {
         if (sliceSize <= warp_size) {
           AT_DISPATCH_V2(
