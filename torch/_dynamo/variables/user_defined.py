394cc7786825643b5eb16d7b2eecacfb9971cb7c
morrison-turnansky <mturnans@redhat.com>
2025-11-18 20:55:13 +0000

    reviwer comments

--- torch/_dynamo/variables/user_defined.py
+++ torch/_dynamo/variables/user_defined.py
@@ -115,6 +115,7 @@ if TYPE_CHECKING:
     from torch._dynamo.symbolic_convert import InstructionTranslator
 
     from .constant import ConstantVariable
+    from .lists import TupleVariable
 
 
 def is_standard_setattr(val):
@@ -2432,17 +2433,11 @@ class UserDefinedTupleVariable(UserDefinedObjectVariable):
     def __init__(
         self,
         value,
-        tuple_vt=None,
+        tuple_vt: Optional["TupleVariable"] = None,
         init_args=None,
         **kwargs,
     ):
         super().__init__(value, init_args=init_args, **kwargs)
-
-        if TYPE_CHECKING:
-            from .lists import TupleVariable
-
-            tuple_vt: Optional[TupleVariable]
-
         if tuple_vt is None:
             assert self.source is None, (
                 "tuple_vt must be constructed by builder.py when source is present"
