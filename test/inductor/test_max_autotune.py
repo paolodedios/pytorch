96d21b8dd3080701938a729378c66d6a547fa823
Jack Taylor <jack.taylor@amd.com>
2025-12-09 06:00:18 -0600

    More unskips

--- test/inductor/test_max_autotune.py
+++ test/inductor/test_max_autotune.py
@@ -1642,7 +1642,7 @@ class TestMaxAutotune(TestCase):
             FileCheck().check("contiguous_mm").run(code[0])
 
     @unittest.skipIf(config.cpp_wrapper, "out_dtype override not supported for AOTI")
-    @unittest.skipIf(TEST_WITH_ROCM, "out_dtype override only available on NVIDIA")
+    @skipIfXpu(msg="out_dtype override only available on NVIDIA")
     def test_bmm_out_dtype(self):
         def f(a, b):
             return torch.bmm(a, b, out_dtype=torch.float32)
