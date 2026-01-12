82dc134561dcdd82fe678daf3a1bf4796f796265
Josh Williams <josh.williams@stfc.ac.uk>
2026-01-12 17:13:00 +0000

    ignore bad argument type for dtype

--- torch/distributed/tensor/_redistribute.py
+++ torch/distributed/tensor/_redistribute.py
@@ -1704,6 +1704,7 @@ def _redistribute_backward(
             tensor_meta=TensorMeta(
                 shape=grad_output.shape,
                 stride=grad_output.stride(),
+                # pyrefly: ignore [bad-argument-type]
                 dtype=backward_dtype,
             ),
         )
