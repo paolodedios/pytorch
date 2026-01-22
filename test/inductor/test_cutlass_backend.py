7a5c5d7e8277fdecded0f7834a434554f3627cd6
Masaki Kozuki <mkozuki@nvidia.com>
2026-01-22 11:25:48 +0900

    Only Hopper does `run_test(True)`
    With the current implementation, Inductor seems to generate CUTLASS
    kernels for FP8 `torch._scaled_mm` on Blackwell but their names would
    not include "fastaccum" even when `use_fast_accum=True`, for example:
    
    ```
    cutlass3x_sm100_tensorop_gemm_e4m3_e4m3_f32_bf16_bf16_64x128x128_0x0x1_0_tnn_align16_1sm
    cutlass3x_sm100_tensorop_gemm_e4m3_e4m3_f32_bf16_bf16_64x128x128_0x0x1_0_tnn_align16_stream_k_1sm
    cutlass3x_sm100_tensorop_gemm_e4m3_e4m3_f32_bf16_e4m3_64x128x128_0x0x1_0_tnn_align16_1sm
    cutlass3x_sm100_tensorop_gemm_e4m3_e4m3_f32_bf16_e4m3_64x128x128_0x0x1_0_tnn_align16_stream_k_1sm
    cutlass3x_sm100_tensorop_gemm_e4m3_e4m3_f32_bf16_bf16_64x128x128_1x1x1_0_tnn_align16_1sm
    ```
    
    while, on Hopper, with `use_fast_accum`,
    
    ```
    cutlass3x_sm90_tensorop_gemm_e4m3_e4m3_f32_f32_f32_64x256x128_2x1x1_0_tnn_align16_warpspecialized_pingpong_fp8_fastaccum_epi_tma
    cutlass3x_sm90_tensorop_gemm_e4m3_e4m3_f32_f32_f32_64x256x128_2x1x1_0_tnn_align16_warpspecialized_fp8_fastaccum_epi_nosmem
    cutlass3x_sm90_tensorop_gemm_e4m3_e4m3_f32_f32_f32_64x256x128_2x1x1_0_tnn_align16_warpspecialized_pingpong_fp8_fastaccum_epi_nosmem
    cutlass3x_sm90_tensorop_gemm_e4m3_e4m3_f32_f32_e4m3_64x256x128_2x1x1_0_tnn_align16_warpspecialized_pingpong_fp8_fastaccum_epi_tma
    cutlass3x_sm90_tensorop_gemm_e4m3_e4m3_f32_f32_e4m3_64x256x128_2x1x1_0_tnn_align16_warpspecialized_fp8_fastaccum_epi_nosmem
    ```
    
    and without `use_fast_accum`,
    
    ```
    cutlass3x_sm90_tensorop_gemm_e4m3_e4m3_f32_bf16_bf16_64x128x128_2x1x1_0_tnn_align16_warpspecialized_pingpong_epi_tma
    cutlass3x_sm90_tensorop_gemm_e4m3_e4m3_f32_bf16_bf16_64x128x128_2x1x1_0_tnn_align16_warpspecialized_epi_nosmem
    cutlass3x_sm90_tensorop_gemm_e4m3_e4m3_f32_bf16_e4m3_64x128x128_2x1x1_0_tnn_align16_warpspecialized_pingpong_epi_tma
    ```
    
    Signed-off-by: Masaki Kozuki <mkozuki@nvidia.com>

--- test/inductor/test_cutlass_backend.py
+++ test/inductor/test_cutlass_backend.py
@@ -45,6 +45,7 @@ from torch.sparse import SparseSemiStructuredTensor, to_sparse_semi_structured
 from torch.testing import FileCheck
 from torch.testing._internal.common_cuda import (
     PLATFORM_SUPPORTS_FP8,
+    SM100OrLater,
     SM80OrLater,
     SM90OrLater,
 )
@@ -1439,7 +1440,8 @@ class TestCutlassBackend(TestCase):
                         if cuda_template_count <= 0:
                             raise AssertionError("No CUTLASSTemplateCaller choices")
 
-        run_test(True)
+        if not SM100OrLater:
+            run_test(True)
         run_test(False)
 
     @unittest.skipIf(not SM90OrLater, "need sm_90")
