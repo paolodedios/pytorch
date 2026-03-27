f4de42daee7cb990b1e6cab7ccc5b3ff2961a998
Bob Ren <bobrenjc93@users.noreply.github.com>
2026-03-27 20:23:25 +0000

    Fix lint and doctest CI fallout

--- torch/_functorch/aot_autograd.py
+++ torch/_functorch/aot_autograd.py
@@ -160,7 +160,7 @@ if TYPE_CHECKING:
 
     from torch._inductor.cudagraph_utils import BoxedDeviceIndex
     from torch._inductor.output_code import OutputCode
-    from torch._inductor.utils import BoxedBool, InputType
+    from torch._inductor.utils import InputType
     from torch._ops import OpOverload
     from torch.fx.experimental.symbolic_shapes import ShapeEnv
 
