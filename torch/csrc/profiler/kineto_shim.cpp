b21f1af5a0b92f6635e89bf9adc0e1f44230a54c
Lakshay Garg <lakshayg@nvidia.com>
2026-04-05 14:54:51 -0700

    Run clang-tidy with readability-container-contains

--- torch/csrc/profiler/kineto_shim.cpp
+++ torch/csrc/profiler/kineto_shim.cpp
@@ -274,15 +274,15 @@ void prepareTrace(
   };
 
   bool has_cpu_activity =
-      activities.count(torch::autograd::profiler::ActivityType::CPU);
+      activities.contains(torch::autograd::profiler::ActivityType::CPU);
 
   if (has_cpu_activity) {
     insertActivities(torch::autograd::profiler::ActivityType::CPU, kCpuTypes);
   }
-  if (activities.count(torch::autograd::profiler::ActivityType::XPU)) {
+  if (activities.contains(torch::autograd::profiler::ActivityType::XPU)) {
     insertActivities(torch::autograd::profiler::ActivityType::XPU, kXpuTypes);
   }
-  if (activities.count(torch::autograd::profiler::ActivityType::MTIA)) {
+  if (activities.contains(torch::autograd::profiler::ActivityType::MTIA)) {
     if (config.custom_profiler_config.empty()) {
       insertActivities(
           torch::autograd::profiler::ActivityType::MTIA, kMtiaTypes);
@@ -313,10 +313,10 @@ void prepareTrace(
       }
     }
   }
-  if (activities.count(torch::autograd::profiler::ActivityType::HPU)) {
+  if (activities.contains(torch::autograd::profiler::ActivityType::HPU)) {
     insertActivities(torch::autograd::profiler::ActivityType::HPU, kHpuTypes);
   }
-  if (activities.count(torch::autograd::profiler::ActivityType::CUDA)) {
+  if (activities.contains(torch::autograd::profiler::ActivityType::CUDA)) {
     insertActivities(torch::autograd::profiler::ActivityType::CUDA, kCudaTypes);
     if (config.enable_cuda_sync_events || get_cuda_sync_enabled()) {
       LOG(INFO) << "Enabling CUDA Sync Events";
@@ -326,7 +326,8 @@ void prepareTrace(
   if (collectivesProfilerExists()) {
     k_activities.insert(libkineto::ActivityType::COLLECTIVE_COMM);
   }
-  if (activities.count(torch::autograd::profiler::ActivityType::PrivateUse1)) {
+  if (activities.contains(
+          torch::autograd::profiler::ActivityType::PrivateUse1)) {
     insertActivities(
         torch::autograd::profiler::ActivityType::PrivateUse1,
         kPrivateUse1Types);
