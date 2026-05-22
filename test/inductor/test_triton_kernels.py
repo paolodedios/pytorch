0c7fc9c463963cefea3ebf24fc389818d4e997e5
Joshua James Venter <venter.joshua@gmail.com>
2026-05-22 14:49:34 +0200

    use tuple as annotation type

--- test/inductor/test_triton_kernels.py
+++ test/inductor/test_triton_kernels.py
@@ -5793,7 +5793,7 @@ class TestUserKernelEpilogueFusion(torch._inductor.test_case.TestCase):
         def fn(a, b):
             out = torch.empty_like(a)
             grid = (triton.cdiv(a.numel(), 1024),)
-            wrap_triton(add_kernel, output_tile={"x": "BLOCK_SIZE"})[grid](
+            wrap_triton(add_kernel, output_tile=("BLOCK_SIZE",))[grid](
                 a, b, out, a.numel(), BLOCK_SIZE=1024
             )
             return out + torch.cumsum(torch.ones_like(out), dim=0)
@@ -5819,7 +5819,7 @@ class TestUserKernelEpilogueFusion(torch._inductor.test_case.TestCase):
         def fn(a, b, c):
             out = torch.empty_like(a)
             grid = (triton.cdiv(a.numel(), 1024),)
-            wrap_triton(add_kernel, output_tile={"x": "BLOCK_SIZE"})[grid](
+            wrap_triton(add_kernel, output_tile=("BLOCK_SIZE",))[grid](
                 a, b, out, a.numel(), BLOCK_SIZE=1024
             )
             out = out * c # Non-unary epilogue
@@ -5850,7 +5850,7 @@ class TestUserKernelEpilogueFusion(torch._inductor.test_case.TestCase):
         def fn(a, b, c):
             out = torch.empty_like(a)
             grid = (triton.cdiv(a.numel(), 1024),)
-            wrap_triton(add_kernel, output_tile={"x": "BLOCK_SIZE"})[grid](
+            wrap_triton(add_kernel, output_tile=("BLOCK_SIZE",))[grid](
                 a, b, out, a.numel(), BLOCK_SIZE=1024
             )
             out = out * c # Non-unary epilogue
