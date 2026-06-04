746391ba16e6be63735a4c9a99e05513e4883598
Angel Li <liangel@meta.com>
2026-06-04 08:20:26 -0700

    more c++ faketensor awareness

--- torch/_subclasses/meta_utils.py
+++ torch/_subclasses/meta_utils.py
@@ -60,9 +60,8 @@ if TYPE_CHECKING:
 
 
 def _is_fake_tensor(t: object) -> TypeIs[FakeTensor]:
-    from torch._subclasses.fake_tensor import FakeTensor
-
-    return isinstance(t, FakeTensor)
+    from torch._subclasses.fake_tensor import is_fake
+    return is_fake(t)
 
 
 def _unwrap_python_functional_tensor(t: torch.Tensor) -> torch.Tensor:
