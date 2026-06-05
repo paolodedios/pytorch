1fa44300ae6ce4b85808cc7b829b1c04bd74ab57
Tirth Patel <tirthp@nvidia.com>
2026-06-04 21:30:38 -0700

    implement the expanded contant fix for internal tensors

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -18625,6 +18625,7 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
 
         self.assertEqual(out_eager, out_compiled)
         self.assertEqual(x_eager.device, x_compiled.device)
+
     def test_jvp_compile_backward(self):
         def jvp_fn(f, x):
             return torch.func.jvp(f, (x.clone(),), (torch.ones_like(x),))[1]
@@ -18680,6 +18681,7 @@ def forward(self, arg0_1: "Sym(s77)", arg1_1: "Sym(s27)", arg2_1: "Sym(s53)", ar
         out = torch.compile(fn, fullgraph=True)()
         self.assertEqual(out.size(0), numel)
         self.assertEqual(out.stride(), (1,))
+        self.assertTrue(out._is_zerotensor())
 
     # end of class CommonTemplate - add new tests here
 
