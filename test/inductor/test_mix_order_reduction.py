25d36a52d95da90aa0b33d66ffbed4d8fcd1f28d
ZhaoqiongZ <zhaoqiong.zheng@intel.com>
2026-06-22 08:08:37 +0000

    fix lint: indent continuation strings to match msg= alignment

--- test/inductor/test_mix_order_reduction.py
+++ test/inductor/test_mix_order_reduction.py
@@ -1163,9 +1163,9 @@ class OverFusionTest(TestBase):
 
     @skipIfXpu(
         msg="XPU Flash Attention backward uses SYCL TLA which does not guarantee "
-"precision on PVC; fails on Ubuntu 24.04 LTS2 (CI) builds with MKL 2024.2. "
-"Will be re-enabled once oneDNN adds training support for this attention "
-"pattern on PVC. See https://github.com/intel/torch-xpu-ops/issues/4094"
+        "precision on PVC; fails on Ubuntu 24.04 LTS2 (CI) builds with MKL 2024.2. "
+        "Will be re-enabled once oneDNN adds training support for this attention "
+        "pattern on PVC. See https://github.com/intel/torch-xpu-ops/issues/4094"
     )
     @inductor_config.patch(
         {
