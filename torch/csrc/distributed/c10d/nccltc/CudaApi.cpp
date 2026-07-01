f8f00fd0365893f4e8e5cd88a50cc0b3b3e43819
Tristan Rice <rice@fn.lc>
2026-06-30 17:23:23 -0700

    Update (base update)
    [ghstack-poisoned]

--- torch/csrc/distributed/c10d/nccltc/CudaApi.cpp
+++ torch/csrc/distributed/c10d/nccltc/CudaApi.cpp
@@ -3,8 +3,6 @@
 #include <ATen/cuda/CUDAContext.h>
 #include <torch/csrc/distributed/c10d/nccltc/CudaApi.hpp>
 
-#include <c10/cuda/driver_api.h>
-
 namespace c10d::nccltc {
 
 // DefaultCudaApi implementation
@@ -207,31 +205,6 @@ cudaError_t DefaultCudaApi::eventQuery(cudaEvent_t event) {
 
 const char* DefaultCudaApi::getErrorString(cudaError_t error) {
   return cudaGetErrorString(error);
-}
-
-#if defined(CUDART_VERSION) && CUDART_VERSION >= 12030
-#define CUDART_SUPPORTS_MULTICAST
-#endif
-
-bool deviceSupportsMulticast(int device_idx) {
-#if defined(CUDART_SUPPORTS_MULTICAST)
-  // Multicast support requirements:
-  // - CUDA Runtime version >= 12030: Checked at compile time using
-  // CUDART_VERSION.
-  // - Driver version >= 535: Checked at runtime by verifying the existence of
-  // cuMulticastCreate_.
-  // - Device support: Determined by querying
-  // CU_DEVICE_ATTRIBUTE_MULTICAST_SUPPORTED at runtime.
-  auto driver_api = c10::cuda::DriverAPI::get();
-  int multicast_supported = 0;
-  C10_CUDA_DRIVER_CHECK(driver_api->cuDeviceGetAttribute_(
-      &multicast_supported,
-      CU_DEVICE_ATTRIBUTE_MULTICAST_SUPPORTED,
-      device_idx));
-  return driver_api->cuMulticastCreate_ != nullptr && multicast_supported;
-#else
-  return false;
-#endif
 }
 
 } // namespace c10d::nccltc
