f3cf9cb6855aefd73549196d97a3553e45de2233
Thomas Bohnstingl <boh@zurich.ibm.com>
2026-07-01 13:43:51 +0200

    Removed any autograd leftovers

--- torch/_higher_order_ops/cond.py
+++ torch/_higher_order_ops/cond.py
@@ -1,6 +1,7 @@
 # mypy: allow-untyped-decorators
 # mypy: allow-untyped-defs
 import contextlib
+import functools
 import logging
 import warnings
 from collections.abc import Callable
@@ -21,7 +22,8 @@ from torch._higher_order_ops.utils import (
     check_input_alias_and_mutation_return_outputs,
     create_bw_fn,
     fill_none_with_masks,
-    materialize_bw_fn_filter_non_tensor_grads,
+    filter_with_masks,
+    materialize_as_graph,
     reenter_make_fx,
     save_values_for_backward,
     saved_values,
@@ -345,17 +347,35 @@ class CondAutogradOp(torch.autograd.Function):
         args = operands + flat_grads
         # TODO: we need to materialize the bw graphs because dynamo is unable to
         # trace through the joint function when torch.compile torch.autograd.grad.
-        true_bw_gm, grads_tensor_masks = materialize_bw_fn_filter_non_tensor_grads(
-            ctx._true_bw_fn,
+
+        grads_tensor_masks = []
+
+        def create_fn_remove_none(fn):
+            @functools.wraps(fn)
+            def wrapped(*args):
+                nonlocal grads_tensor_masks
+
+                true_outputs = fn(*args)
+                grads_tensor_masks = [
+                    bool(isinstance(out, torch.Tensor)) for out in true_outputs
+                ]
+                return filter_with_masks(true_outputs, grads_tensor_masks)
+
+            return wrapped
+
+        true_bw_gm = materialize_as_graph(
+            create_fn_remove_none(ctx._true_bw_fn),
             args,
             ctx._fw_include_key_set,
             ctx._fw_exclude_key_set,
+            force_enable_grad=True,
         )
-        false_bw_gm, _ = materialize_bw_fn_filter_non_tensor_grads(
-            ctx._false_bw_fn,
+        false_bw_gm = materialize_as_graph(
+            create_fn_remove_none(ctx._false_bw_fn),
             args,
             ctx._fw_include_key_set,
             ctx._fw_exclude_key_set,
+            force_enable_grad=True,
         )
         grads = cond_op(
             ctx._pred,
